71b2331980823398533dea240274219e727f957e
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2025-02-27 13:41:50 +0100

    QURIO-3: Initialiseer backend
    - Project gegenereerd met Spring Initializr

---
+++ src/main/java/com/qurio/qurio/QurioApplication.java
@@ -0,0 +1,13 @@
+package com.qurio.qurio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QurioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QurioApplication.class, args);
+	}
+
+}
