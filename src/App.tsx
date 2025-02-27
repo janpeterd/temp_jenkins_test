d6083e68ea73c00335d1e5b51c3acf7f2036b073
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2025-02-27 12:00:59 +0100

    QURIO-3 Initialiseer frontend
    - Dependencies geïnstalleerd
    - React initialisatie met Vite
    - Tailwind initialisatie
    - Shadcn/UI initialisatie

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-5xl font-bold">Hello Qurio</h1>
+    </div>
+  );
+}
+
+export default App;
